539787ea61111ff7c8c14032bec5559ca4927dc6
franktominc <frankptominc@gmail.com>
2016-05-20 19:36:47 -0300

    finally managed to get pid

--- wow.asm
+++ wow.asm
@@ -1,7 +1,18 @@
 global _start:
 
+section .data
+
+section .bss
+
+  pid: resq 1
+
 section .text
 
 _start:
 
-mov XMM0, 0x7f7f7f7f7f
+mov rax, 39d
+syscall
+mov [pid], rax
+breakpoint:
+mov rax, 0
+syscall
