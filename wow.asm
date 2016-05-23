bbbdb819efdfe99f5292ecd5c3159346e98f8b85
franktominc <frankptominc@gmail.com>
2016-05-23 01:01:11 -0300

    self killing code

--- wow.asm
+++ wow.asm
@@ -1,7 +1,8 @@
 global _start:
 
 section .data
-
+    strOla : db "this isn’t working!", 10
+    strOlaL: equ $ - strOla
 section .bss
 
   pid: resq 1
@@ -10,9 +11,22 @@ section .text
 
 _start:
 
+;syscall get pid
 mov rax, 39d
 syscall
 mov [pid], rax
+;syscall kill
+mov rax, 62d
+mov rdi, [pid]
+mov rsi, 9d
+syscall
+;syscall write
+mov rax, 1
+mov rdi, 1
+mov rsi, strOla
+mov rdx, strOlaL
+syscall
 breakpoint:
-mov rax, 0
+mov rax, 60d
+xor rdi, rdi
 syscall
